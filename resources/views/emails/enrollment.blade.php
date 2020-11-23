ccc041c41a0b5419f745b1f5db84caeaa550c000
AlexanderHOB <obispoa1@gmail.com>
2020-11-23 14:48:33 -0500

    test image

--- resources/views/emails/enrollment.blade.php
+++ resources/views/emails/enrollment.blade.php
@@ -2,10 +2,13 @@
 @component('mail::message')
 # Hola {{$user->name}}
 Tu matrícula ha sido procesada con éxito.
-Sus credenciales para el aula virtual 
+
+## Sus credenciales para el aula virtual son los siguientes: 
+<br/>
 ## <b>Usuario: {{$student->dni}}</b>
 ## <b>Contraseña: {{$student->phone}}</b>
 
+{{ HTML::image('assets/images/vouchers/1.jpg', 'alt text', array('class' => 'css-class')) }}
 
 @component('mail::button', ['url' => 'https://aula-virtual.academia-el-tambo.teampixeland.com/'])
 Aula Virtual 
